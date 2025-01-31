e22841d3c2a9295425cca319d22bb241cc9b4e7c
Xavier Roca <xavirope20@gmail.com>
2025-01-31 18:28:47 +0100

    ex05 done

---
+++ cpp-01/ex05/Harl.cpp
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xavi <xavi@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 17:55:14 by xavi              #+#    #+#             */
+/*   Updated: 2025/01/31 18:27:42 by xavi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl() {}
+
+Harl::~Harl() {}
+
+void Harl::debug() {
+    std::cout << "[DEBUG] I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void Harl::info() {
+    std::cout << "[INFO] I cannot believe adding extra bacon costs more money. You didn’t put \
+enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void Harl::warning() {
+    std::cout << "[WARNING] I think I deserve to have some extra bacon for free. I’ve been coming for \
+years whereas you started working here since last month." << std::endl;
+}
+
+void Harl::error() {
+    std::cout << "[ERROR] This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void Harl::complain(std::string level) {
+    void (Harl::*functions[4])() = {&Harl::debug, &Harl::info, &Harl::warning, &Harl::error};
+    std::string levels[4] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+    
+    for (int i = 0; i < 4; i++) {
+        if (levels[i] == level) {
+            (this->*functions[i])();
+            return;  
+        }
+    }
+    std::cout << "[UNKNOWN] To the moon with this burger!! I want it, I want it!!!" << std::endl;
+}
---
+++ cpp-01/ex05/Harl.hpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xavi <xavi@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/31 17:55:11 by xavi              #+#    #+#             */
+/*   Updated: 2025/01/31 18:18:25 by xavi             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+#define HARL_HPP
+
+#include <iostream>
+#include <string>
+
+class Harl {
+    private:
+        void debug(void);
+        void info(void);
+        void warning(void);
+        void error(void);
+
+    public:
+        Harl();
+        ~Harl();
+        void complain(std::string level);
+};
+
+#endif

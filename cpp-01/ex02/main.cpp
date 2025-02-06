41040be3bf1366051febfe9614accf934605066d
Xavier Roca <xavirope20@gmail.com>
2025-02-06 18:16:11 +0100

    42 header

--- cpp-01/ex02/main.cpp
+++ cpp-01/ex02/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: xavi <xavi@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: xroca-pe <xroca-pe@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/26 19:51:43 by xavi              #+#    #+#             */
-/*   Updated: 2025/01/26 20:09:28 by xavi             ###   ########.fr       */
+/*   Updated: 2025/02/06 18:06:04 by xroca-pe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,8 @@
 
 int main () {
     std::string str = "HI THIS IS BRAIN";
-    std::string* stringPTR = &str;  // Pointer to str
-    std::string& stringREF = str;   // Reference to str
+    std::string* stringPTR = &str;
+    std::string& stringREF = str;
 
     std::cout << "Adress of str: " << &str << std::endl;
     std::cout << "Adress of stringPTR: " << stringPTR << std::endl;

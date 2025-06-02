d59013856ceb515a3afed81875725d10647a8c18
Xavier Roca <xavirope20@gmail.com>
2025-06-02 21:01:30 +0200

    cpp06 ex02 done

---
+++ cpp-06/ex02/main.cpp
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xroca-pe <xroca-pe@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/02 20:18:44 by xroca-pe          #+#    #+#             */
+/*   Updated: 2025/06/02 20:58:11 by xroca-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+#include <cstdlib>
+
+Base *generate() {
+    int random = std::rand() % 3;
+    if (random == 0)
+        return new A();
+    else if (random == 1)
+        return new B();
+    else
+        return new C();
+}
+
+void identify(Base *p) {
+    if (dynamic_cast<A*>(p)) {
+        std::cout << "A" << std::endl;
+    } else if (dynamic_cast<B*>(p)) {
+        std::cout << "B" << std::endl;
+    } else if (dynamic_cast<C*>(p)) {
+        std::cout << "C" << std::endl;
+    } else 
+        std::cout << "Unknown type" << std::endl;
+}
+
+void identify(Base &p) {
+    try {
+        dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+    }
+    catch (std::exception &e) {
+        //std::cerr << "Not A" << std::endl;
+    }
+    try {
+        dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+    }
+    catch (std::exception &e) {
+        //std::cerr << "Not B" << std::endl;
+    }
+    try {
+        dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+    }
+    catch (std::exception &e) {
+        //std::cerr << "Not C" << std::endl;
+    }    
+}
+
+int main() {
+    std::cout << "Identifying randomly generated Base objects:" << std::endl;
+    std::srand(static_cast<unsigned int>(time(NULL)));
+    for (int i = 0; i < 10; ++i) {
+        Base *base = generate();
+        identify(base);
+        identify(*base);
+        delete base;
+        std::cout << std::endl;
+    }
+    
+    std::cout << "Identifying null Base pointer and reference:" << std::endl;
+    identify(NULL);
+    std::cout << std::endl;
+    std::cout << "Identifying wrong Base reference:" << std::endl;
+    Base z;
+    identify(z);
+    
+    return 0;
+}

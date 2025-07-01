824ec7051aee8fc5ecd0fcf6468b7a0f4d17d8aa
Xavier Roca <xavirope20@gmail.com>
2025-07-01 20:50:45 +0200

    cpp8 ex00 done

---
+++ cpp-08/ex00/easyfind.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   easyfind.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xroca-pe <xroca-pe@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 20:19:17 by xroca-pe          #+#    #+#             */
+/*   Updated: 2025/07/01 20:19:25 by xroca-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#include <algorithm>
+#include <stdexcept>
+
+template <typename T>
+typename T::iterator easyfind(T &container, int value) {
+    typename T::iterator it = std::find(container.begin(), container.end(), value);
+    if (it == container.end()) {
+        throw std::runtime_error("Value not found in the container");
+    }
+    return it;
+}
+
+#endif
---
+++ cpp-08/ex00/main.cpp
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xroca-pe <xroca-pe@student.42barcel>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/07/01 20:14:08 by xroca-pe          #+#    #+#             */
+/*   Updated: 2025/07/01 20:38:51 by xroca-pe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include <iterator>
+#include "easyfind.hpp"
+
+int main() {
+    std::vector<int> vec;
+    std::list<int> lst;
+    for (int i = 1; i <= 10; ++i) {
+        vec.push_back(i);
+        lst.push_back(i);
+    }
+    
+    try {
+        std::vector<int>::iterator it = easyfind(vec, 3);
+        std::cout << "Found " << *it << " in vector at position " << std::distance(vec.begin(), it) << std::endl;
+    }
+    catch (const std::runtime_error &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    try {
+        std::list<int>::iterator it = easyfind(lst, 7);
+        std::cout << "Found " << *it << " in list at position " << std::distance(lst.begin(), it) << std::endl;
+    }
+    catch (const std::runtime_error &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    try{
+        std::vector<int>::iterator it = easyfind(vec, 42);
+        std::cout << "Found " << *it << " in vector at position " << std::distance(vec.begin(), it) << std::endl;
+    }
+    catch (const std::runtime_error &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    try{
+        std::list<int>::iterator it = easyfind(lst, 42);
+        std::cout << "Found " << *it << " in list at position " << std::distance(lst.begin(), it) << std::endl;
+    }
+    catch (const std::runtime_error &e) {
+        std::cerr << e.what() << std::endl;
+    }
+
+    return 0;    
+}
